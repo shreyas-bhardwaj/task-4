7a784efd2eae5214ef245a76c0b7e3cb5cc0e551
Shreyas Bhardwaj <shreyas.bhardwaj@gmail.com>
2023-06-21 17:26:03 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((res) => setUsers(res.users))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <h1>Users</h1>
+          <table class="table table-dark">
+            <thead>
+            <tr>
+              <th scope="col">Username</th>
+              <th scope="col">Age</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone Number</th>
+            </tr>
+            </thead>
+            {users.map(user => (
+              <tbody>
+
+              <tr key={user.id}>
+                <td>{user.username}</td>
+                <td>{user.age}</td>
+                <td>{user.gender}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+              </tr>
+              </tbody>
+            ))}
+          </table>
+        </>
+      )}
     </div>
   );
 }
